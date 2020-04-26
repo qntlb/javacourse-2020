e2fefc5535abbb7bebca71437410174853e518ea
Andrea Mazzon <mazzon@math.lmu.de>
2020-04-26 10:18:54 +0200

    Small change to a comment

--- src/main/java/com/andreamazzon/session6/generics/genericinterfaces/GenericPointer.java
+++ src/main/java/com/andreamazzon/session6/generics/genericinterfaces/GenericPointer.java
@@ -2,7 +2,7 @@ package com.andreamazzon.session6.generics.genericinterfaces;
 
 /**
  * All classes implementing this interface must implement a generic method next(), which return an object.
- * We do not specify now the type of the object reterned, but it has to be the same for every object calling
+ * We do not specify now the type of the object returned, but it has to be the same for every object calling
  * the method.
  *
  * @author Andrea Mazzon
