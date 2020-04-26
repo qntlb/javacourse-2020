57e0bb6a31525070cbbf615455c757c9a1a49b96
Andrea Mazzon <mazzon@math.lmu.de>
2020-04-26 12:08:39 +0200

    More comments on private, inner classes

--- src/main/java/com/andreamazzon/session6/innerclasses/privateinnerclasses/package-info.java
+++ src/main/java/com/andreamazzon/session6/innerclasses/privateinnerclasses/package-info.java
@@ -2,6 +2,8 @@
  * Here we have one more example on the use of inner classes. In particular, the inner class
  * SequenceSelector of Sequence implements the interface Selector. A reference to this subclass
  * is returned by the method  getSequenceSelector() of Sequence. In this way, the user only sees
- * the methods of Selector implemented by SequenceSelector, and not the class itself (since it is private)
+ * the methods of Selector implemented by SequenceSelector, and not the class itself, because it is private.
+ * Note that private classes only make sense if they are inner classes. Otherwise objects of such classes
+ * cannot be created.
  */
 package com.andreamazzon.session6.innerclasses.privateinnerclasses;
