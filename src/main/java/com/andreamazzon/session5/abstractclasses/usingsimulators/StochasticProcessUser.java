dbf13638e436681f0211fe2c4fbf7ddc57167b9d
Andrea Mazzon <mazzon@math.lmu.de>
2020-04-27 09:42:09 +0200

    Added the interface

--- src/main/java/com/andreamazzon/session5/abstractclasses/usingsimulators/StochasticProcessUser.java
+++ src/main/java/com/andreamazzon/session5/abstractclasses/usingsimulators/StochasticProcessUser.java
@@ -5,7 +5,7 @@ import com.andreamazzon.session4.usefulmatrices.UsefulMethodsMatricesVectors;
 
 /**
  * This class has methods to get the paths, the simulated value at a given time and the average value
- * at given time of a general stochastic process.
+ * at given time of a general stochastic process S.
  * The implementation of the methods doing this is given, but is based on the generation of the realizations
  * of the process. Since this is based on the specific type of the process, it gets implemented in the
  * derived classes and is therefore abstract.
@@ -13,7 +13,7 @@ import com.andreamazzon.session4.usefulmatrices.UsefulMethodsMatricesVectors;
  * @author Andrea Mazzon
  *
  */
-public abstract class StochasticProcessUser {
+public abstract class StochasticProcessUser implements StochasticProcessUserInterface{
 
 	//realizations of S. The matrix will be filled by the getRealizations() method
 	protected double[][] realizations;
@@ -26,6 +26,7 @@ public abstract class StochasticProcessUser {
 	 * @param time, the time i such that the simulated values of S(i) are returned
 	 * @return the simulated values of S at time time.
 	 */
+	@Override
 	public double[] getRealizationsAtGivenTime(int time) {
 		generateRealizations();//realizations generated only when needed: lazy initialization
 		return UsefulMethodsMatricesVectors.getRow(realizations, time);
@@ -35,6 +36,7 @@ public abstract class StochasticProcessUser {
 	 * It prints the vector of realizations at time time.
 	 * @param time, the time i such that the simulated values of S(i) are printed
 	 */
+	@Override
 	public void printRealizationsAtGivenTime(int time) {
 		UsefulMethodsMatricesVectors.printVector(getRealizationsAtGivenTime(time));
 	}
@@ -43,6 +45,7 @@ public abstract class StochasticProcessUser {
 	 * @param simulationIndex, the index of the simulation for which the path is returned
 	 * @return the path of S for the specific simulation index
 	 */
+	@Override
 	public double[] getPath(int simulationIndex) {
 		generateRealizations();//realizations generated only when needed: lazy initialization
 		return UsefulMethodsMatricesVectors.getColumn(realizations, simulationIndex);
@@ -52,6 +55,7 @@ public abstract class StochasticProcessUser {
 	 * it prints the path of S for the specific simulation index
 	 * @param simulationIndex, the index of the simulation for which the path is returned
 	 */
+	@Override
 	public void printPath(int simulationIndex) {
 		UsefulMethodsMatricesVectors.printVector(getPath(simulationIndex));
 	}
@@ -60,6 +64,7 @@ public abstract class StochasticProcessUser {
 	 * @param time, the time i such that the simulated values of S(i) are returned
 	 * @return the average of S at time time.
 	 */
+	@Override
 	public double getAverageAtGiveTime(int time) {
 		return UsefulMethodsMatricesVectors.getAverage(getRealizationsAtGivenTime(time));
 	}
