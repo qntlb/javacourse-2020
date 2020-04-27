a80a0b65c29708b6d929bb4f55922568473905d7
Andrea Mazzon <mazzon@math.lmu.de>
2020-04-27 11:49:43 +0200

    Made the print() methods of the exceptions public

--- src/main/java/com/andreamazzon/session6/exceptions/divisions/DivideByZeroException.java
+++ src/main/java/com/andreamazzon/session6/exceptions/divisions/DivideByZeroException.java
@@ -3,7 +3,7 @@ package com.andreamazzon.session6.exceptions.divisions;
 //Exception is a class, when you write an Exception you extend it
 @SuppressWarnings("serial")
 public class DivideByZeroException extends Exception{
-	void print() {//method you want to call when you catch the exception
+	public void print() {//method you want to call when you catch the exception
 		System.out.println("You are dividing by zero");
 	}
 }
--- src/main/java/com/andreamazzon/session6/exceptions/divisions/UnderFlowException.java
+++ src/main/java/com/andreamazzon/session6/exceptions/divisions/UnderFlowException.java
@@ -3,7 +3,7 @@ package com.andreamazzon.session6.exceptions.divisions;
 //Exception is a class, when you write an Exception you extend it
 @SuppressWarnings("serial")
 public class UnderFlowException extends Exception { // not an important warning
-	void print() {//method you want to call when you catch the exception
-		System.out.println("I catched an undeflow exception");
+	public void print() {//method you want to call when you catch the exception
+		System.out.println("I catched an underflow exception");
 	}
 }
