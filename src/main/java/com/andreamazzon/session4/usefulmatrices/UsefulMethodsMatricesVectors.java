615d51f599f64e097aaa07e304c3d3152a829636
Andrea Mazzon <mazzon@math.lmu.de>
2020-05-02 14:46:51 +0200

    Added methods to compute the min, max and histogram of a vector

--- src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
+++ src/main/java/com/andreamazzon/session4/usefulmatrices/UsefulMethodsMatricesVectors.java
@@ -1,29 +1,30 @@
 package com.andreamazzon.session4.usefulmatrices;
 
 /**
- * This class contains some useful static methods for dealing with matrices and arrays
+ * This class contains some useful static methods for dealing with matrices and
+ * arrays
  *
  * @author Andrea Mazzon
  *
  */
 public class UsefulMethodsMatricesVectors {
 
-	//a matrix is an array of arrays
+	// a matrix is an array of arrays
 
 	/**
 	 *
 	 * @param matrix
 	 * @return transpose of matrix
 	 */
-	public static double[][] getTranspose (double matrix[][]){
+	public static double[][] getTranspose(double matrix[][]) {
 		int columnLength = matrix.length;
-		//matrix[0] is the first row of the matrix
-		int rowLength = matrix[0].length;//number of columns: length of the row
+		// matrix[0] is the first row of the matrix
+		int rowLength = matrix[0].length;// number of columns: length of the row
 		double[][] transpose = new double[rowLength][columnLength];
 
-		//double for loop to transpose the matrix
+		// double for loop to transpose the matrix
 		for (int i = 0; i < rowLength; i++) {
-			for (int j = 0; j < columnLength; j++){
+			for (int j = 0; j < columnLength; j++) {
 				transpose[i][j] = matrix[j][i];
 			}
 		}
@@ -38,7 +39,7 @@ public class UsefulMethodsMatricesVectors {
 	 */
 	public static double[] getColumn(double[][] matrix, int columnIndex) {
 		double[][] transpose = getTranspose(matrix);
-		return transpose[columnIndex];//the row of the transpose indicated by columnIndex
+		return transpose[columnIndex];// the row of the transpose indicated by columnIndex
 	}
 
 	/**
@@ -62,16 +63,83 @@ public class UsefulMethodsMatricesVectors {
 		for (int i = 0; i < length; i++) {
 			sum += vector[i];
 		}
-		return sum/length;
+		return sum / length;
 	}
 
 	/**
 	 * it prints the entries of vector
+	 *
 	 * @param vector
 	 */
 	public static void printVector(double[] vector) {
-		for (int i = 0; i< vector.length; i ++) {
+		for (int i = 0; i < vector.length; i++) {
 			System.out.println(vector[i]);
 		}
+	}
+
+	/**
+	 * It returns the biggest element of a one-dimensional array of doubles
+	 * 
+	 * @param vector the one-dimensional array
+	 * @return the biggest element of the one-dimensional array
+	 */
+	public static double getMin(double[] vector) {
+		double min = vector[0];
+		for (int i = 1; i < vector.length; i++) {
+			if (vector[i] < min) {
+				min = vector[i];
+			}
+		}
+		return min;
+	}
+
+	/**
+	 * It returns the biggest element of a one-dimensional array of doubles
+	 * 
+	 * @param vector the one-dimensional array
+	 * @return the biggest element of the one-dimensional array
+	 */
+	public static double getMax(double[] vector) {
+		double max = vector[0];
+		for (int i = 1; i < vector.length; i++) {
+			if (vector[i] > max) {
+				max = vector[i];
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * It returns an array of integers which represent the number of realizations of
+	 * a given array that lie in every subinterval (bin) of an interval [minBin,
+	 * maxBin]. All the subintervals are of equal length. The first entry of the
+	 * integer array represent the number of realizations strictly smaller then
+	 * minBin, the last one the number of realizations bigger or equal then maxBin.
+	 *
+	 * @param realisations
+	 * @param minBin
+	 * @param maxBin
+	 * @param binsNumber,  number of the subintervals
+	 * @return array of integers
+	 */
+	public static int[] buildHistogram(double[] realizations, double minBin, double maxBin, int binsNumber) {
+		int[] bins = new int[binsNumber + 2];// two more because of outliers
+		double binSize = (maxBin - minBin) / binsNumber; // every bin has the same length
+		for (double realization : realizations) {
+			if (realization < minBin) { // it goes in the first bin
+				bins[0] += 1;
+			} else if (realization > maxBin) {
+				bins[binsNumber + 1] += 1; // it goes in the last bin
+			} else {
+				int bin = (int) ((realization - minBin) / binSize);
+				/*
+				 * bin[i+1] (remember: in Java the first entry is 0, and here bin[0] hosts the
+				 * realization smaller than min) hosts the realization such that (realization -
+				 * minBin) / binSize is in [i,i+1)
+				 */
+				bins[bin + 1] += 1;
+			}
+		}
+		return bins;
 	}
 }
